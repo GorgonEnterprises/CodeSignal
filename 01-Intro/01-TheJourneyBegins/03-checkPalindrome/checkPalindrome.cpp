2f9400a30df559f6a06185c71742f432461655e9
GorgonEnterprises <50527129+GorgonEnterprises@users.noreply.github.com>
2019-05-12 10:32:21 -0400

    Create checkPalindrome.cpp

---
+++ 01-Intro/01-TheJourneyBegins/03-checkPalindrome/checkPalindrome.cpp
@@ -0,0 +1,7 @@
+bool checkPalindrome(std::string inputString) {
+    for(int i=0; i<inputString.size()/2+1; i++){
+        if(inputString[i] != inputString[inputString.size()-1-i])
+            return false;
+    }
+    return true;
+}
